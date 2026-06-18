c9afad151423ca357a744c590c9e64314e324f17
Solomiia Sakhnik <solomiia.sakhnik@gmail.com>
2026-06-18 11:22:23 +0300

    Add nvim-dap

--- init.lua
+++ init.lua
@@ -78,4 +78,12 @@ vim.pack.add {
 
 require 'nvim-autopairs'.setup {}
 
+vim.pack.add {
+    { src = 'https://github.com/mfussenegger/nvim-dap', },
+    { src = 'https://github.com/mfussenegger/nvim-dap-python', },
+    { src = "https://github.com/igorlfs/nvim-dap-view", version = vim.version.range("1.*")  },
+}
+
+require("dap-python").setup("python3")
+
 print("hello Solia!")
