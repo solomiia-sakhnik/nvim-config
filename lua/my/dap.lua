0fad2a59c4c2de1000355a4da025849a3579fc63
Solomiia Sakhnik <solomiia.sakhnik@gmail.com>
2026-06-18 11:30:15 +0300

    Add debugging utils

--- lua/my/dap.lua
+++ lua/my/dap.lua
@@ -1,8 +1,120 @@
 vim.pack.add {
-    { src = 'https://github.com/mfussenegger/nvim-dap', },
-    { src = 'https://github.com/mfussenegger/nvim-dap-python', },
-    { src = "https://github.com/igorlfs/nvim-dap-view", version = vim.version.range("1.*")  },
+  { src = 'https://github.com/mfussenegger/nvim-dap', },
+  { src = 'https://github.com/nvim-neotest/nvim-nio', },
+  { src = 'https://github.com/igorlfs/nvim-dap-view', },
+  { src = 'https://github.com/mfussenegger/nvim-dap-python', },
+  { src = 'https://github.com/jbyuki/one-small-step-for-vimkind', },
 }
 
-require("dap-python").setup("python3")
+-- Left click to print the symbol (hover)
+local function on_left_click()
+  local mpos = vim.fn.getmousepos()
+  if vim.api.nvim_win_is_valid(mpos.winid) and mpos.line > 0 and mpos.column > 0 then
+    local buf = vim.api.nvim_win_get_buf(mpos.winid)
+    if vim.fn.buflisted(buf) ~= 0 then
+      local ok, lines = pcall(vim.api.nvim_buf_get_lines, buf, mpos.line - 1, mpos.line, true)
+      if ok and #lines > 0 and mpos.column <= #lines[1] then
+        vim.api.nvim_set_current_win(mpos.winid)
+        if pcall(vim.api.nvim_win_set_cursor, mpos.winid, {mpos.line, mpos.column - 1}) then
+          vim.cmd('DapViewHover')
+          return
+        end
+      end
+    end
+  end
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<LeftMouse>", true, true, true), "n", true)
+end
 
+local last_config = nil
+
+local function configure_dap()
+  local dap = require('dap')
+  local timer_id = -1
+
+  ---@param session dap.Session
+  dap.listeners.after.event_initialized["store_config"] = function(session)
+    last_config = session.config
+  end
+
+  local function restore_keymaps()
+    vim.keymap.del('n', '<LeftMouse>')
+    vim.keymap.del('n', '<f4>')
+    vim.keymap.del('n', '<f5>')
+    vim.keymap.del('n', '<f8>')
+    vim.keymap.del('n', '<f9>')
+    vim.keymap.del('n', '<f10>')
+    vim.keymap.del('n', '<f11>')
+    vim.keymap.del('n', '<f12>')
+    vim.keymap.del('n', '<down>')
+    vim.keymap.del('n', '<right>')
+    vim.keymap.del('n', '<left>')
+  end
+
+  local function check_restore_keymaps()
+    if not next(dap.sessions()) then
+      restore_keymaps()
+      vim.fn.timer_stop(timer_id)
+      timer_id = -1
+    end
+  end
+
+  dap.listeners.after['event_initialized']['me'] = function()
+    if timer_id == -1 then
+      -- Set keymaps like in nvim-gdb
+      vim.keymap.set('n', '<f4>', require'dap'.run_to_cursor, { silent = true })
+      vim.keymap.set('n', '<f5>', require'dap'.continue, { silent = true })
+      vim.keymap.set('n', '<f8>', require'dap'.set_breakpoint, { silent = true })
+      vim.keymap.set('n', '<f9>', '<Cmd>DapViewHover<cr>', { silent = true })
+      vim.keymap.set('n', '<f10>', require'dap'.step_over, { silent = true })
+      vim.keymap.set('n', '<down>', require'dap'.step_over, { silent = true })
+      vim.keymap.set('n', '<f11>', require'dap'.step_into, { silent = true })
+      vim.keymap.set('n', '<right>', require'dap'.step_into, { silent = true })
+      vim.keymap.set('n', '<f12>', require'dap'.step_out, { silent = true })
+      vim.keymap.set('n', '<left>', require'dap'.step_out, { silent = true })
+      vim.keymap.set('n', '<LeftMouse>', on_left_click, { silent = true })
+
+      timer_id = vim.fn.timer_start(1000, check_restore_keymaps)
+    end
+  end
+
+  dap.listeners.after['event_terminated']['me'] = check_restore_keymaps
+end
+
+local configured = false
+
+local function get(module)
+  if not configured then
+    configured = true
+
+    configure_dap()
+    require'dapcfg.lua'
+    if vim.fn.executable('gdb') == 1 then
+      require('dapcfg.cpp')
+    end
+  end
+  return require(module)
+end
+
+local function debug_last_session()
+  if last_config then
+    get('dap').run(last_config)
+  else
+    get('dap').continue()
+  end
+end
+
+vim.keymap.set('n', '<leader>bb', function() get('dap').toggle_breakpoint() end, {noremap = true, silent = true, desc = 'DAP toggle breakpoint'})
+vim.keymap.set('n', '<leader>bc', debug_last_session, {noremap = true, silent = true, desc = 'DAP last session'})
+vim.keymap.set('n', '<leader>bC', function() get('dap').continue() end, {noremap = true, silent = true, desc = 'DAP continue'})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    get('dap-python').setup('python3')
+  end,
+})
+
+vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointCondition', {text='❓', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapLogPoint', {text='📝', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', {text='❌', texthl='', linehl='', numhl=''})
